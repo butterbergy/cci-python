4109ae1bed70c1be4800e3dd91b43fcbf8d8095a
Eric Berglund <ericsamuelberglund@gmail.com>
2020-02-23 20:06:47 -0700

    urlify implementation

--- 1_Arrays_and_Strings/3_URLify.py
+++ 1_Arrays_and_Strings/3_URLify.py
@@ -1,3 +1,10 @@
 # Write a method to replace all spaces in a string with '%20'
 # You may assume the string has sufficient space at the end to hold the additional chars,
 # and that you are given the "true" length of the string
+
+def urlify(s):
+    return s.replace(" ", "%20")
+
+print(urlify("asdfasdfasdf"))
+print(urlify("asdf asdf asdf"))
+print(urlify("asdf38g 339g"))
